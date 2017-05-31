8d0e479faffb0faa2ee17cf20e4efb45a1423863
Thomas Powell <t.powell.meto@gmail.com>
2017-05-31 15:16:12 +0100

    add server and client links

--- src/public/js/websocket/shared.js
+++ src/public/js/websocket/shared.js
@@ -69,24 +69,3 @@ function displayPhotosphere (msg) {
     document.getElementById("camera").appendChild(dataHud);
 
 }
-
-AFRAME.registerComponent('poi', {
-    schema: {},
-    init: function () {
-        var self = this;
-        self. code = genServerCode();
-        self.socket = io();
-
-        self.socket.emit("register", {type: "server", code: self.code});
-
-        self.socket.on("display", displayPhotosphere);
-
-        document.getElementById("sid").innerHTML = self.code;
-
-        document.getElementById("serve").addEventListener("click", function () {
-            var pid = document.getElementById("pid").value;
-            console.log("serving %s", pid);
-            self.socket.emit("serve", {id: pid});
-        });
-    }
-});
